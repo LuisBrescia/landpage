8e6f51ad0f8b3b457eab9fe89f0db56424a33811
Luis Brescia <luisfelipebrescia@gmail.com>
2023-09-03 21:22:27 -0300

    Navbar agora continua completa

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,15 +1,15 @@
 <template>
-    <nav class="flex justify-center p-3 sticky top-0 mt-5 z-50">
+    <nav class="flex justify-center p-3 sticky top-0 mt-5 z-50 bg-white" :class="{ 'shadow': isScrolled }">
         <div class="container flex justify-between items-center">
 
             <div class="flex-1 flex items-center gap-1">
                 <!-- <Icon name="lucide:door-open" size="1.2em"></Icon> -->
-                <header id="nomeMarca" class="text-gray-950 px-5 rounded-md bg-white" :class="{ 'shadow': isScrolled }">
+                <header id="nomeMarca" class="text-gray-950 px-5 rounded-md bg-white">
                   atende<span class="text-blue-gradient">aí</span>
                 </header>
             </div>
 
-            <div class="hidden lg:flex gap-4 bg-white rounded-md p-1" :class="{ 'shadow': isScrolled }">
+            <div class="hidden lg:flex gap-4">
                 <a href="/" class="px-3 py-2 rounded-lg hover:bg-gray-100">Como funciona</a>
                 <a href="/" class="px-3 py-2 rounded-lg hover:bg-gray-100">Benefícios</a>
                 <a href="/" class="px-3 py-2 rounded-lg hover:bg-gray-100">Contato</a>
