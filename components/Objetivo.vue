37dc3c969790fac303e44e845cd8d301950410d3
Luis Brescia <luisfelipebrescia@gmail.com>
2023-09-03 21:01:36 -0300

    Novos componentes adicionados

---
+++ components/Objetivo.vue
@@ -0,0 +1,19 @@
+<template>
+
+
+    <header class="container mx-auto text-center max-w-2xl">
+
+        <Icon name="fluent:target-arrow-20-regular" class="mb-3 text-white rounded-md border-2 shadow" size="4em"></Icon>
+
+        <h3 class="font-semibold text-2xl text-white mb-12">
+            Nossa missão é desenvolver e fornecer 
+            <span class="font-black">soluções tecnológicas</span> 
+            que garantam a segurança das pessoas e 
+            <span class="font-black">simplifiquem suas vidas.</span>
+        </h3>
+
+        <button class="btn-escuro">Saiba mais</button>
+    </header>
+
+
+</template>
---
+++ components/Solutions.vue
@@ -0,0 +1,27 @@
+<template>
+    <main class="container mx-auto">
+        <header class="text-4xl font-extrabold text-center mb-12">
+            Conheça nossas <span class="text-blue-gradient">soluções</span>
+        </header>
+
+        <div class="mb-12 flex justify-center gap-10">
+
+            <div class="max-w-lg mb-4">
+                <img src="~/assets/img/solution1.jpg" class="h-72 w-full rounded-lg mb-4 object-cover" />
+                <h3 class="font-semibold text-2xl mb-3">Portaria Autônoma</h3>
+                <p class="text-lg font-light">Oferecemos aos nossos clientes autonomia no controle e acesso do seu condomínio</p>
+            </div>
+
+            <div class="max-w-lg mb-4">
+                <img src="~/assets/img/solution2.jpg" class="h-72 w-full rounded-lg mb-4 object-cover" />
+                <h3 class="font-semibold text-2xl mb-3">App já disponível</h3>
+                <p class="text-lg font-light">Oferecemos aos nossos clientes autonomia no controle e acesso do seu condomínio</p>
+            </div>
+
+        </div>
+
+        <div class="text-center mb-12">
+            <button class="btn-primario">Saiba mais</button>
+        </div>
+    </main>
+</template>
