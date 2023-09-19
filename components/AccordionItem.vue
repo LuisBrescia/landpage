59f2707393a6c86a8d128109c2906ac14ca5f8c7
Luis Brescia <luisfelipebrescia@gmail.com>
2023-09-19 10:53:39 -0300

    AccordionItem.vue adicionado

---
+++ components/AccordionItem.vue
@@ -0,0 +1,42 @@
+<template>
+    <header
+        class="shadow-sm bg-blue-gradient-accordion text-white p-3 rounded-lg text-lg font-medium cursor-pointer flex justify-between items-center"
+        @click="accordionAberto = !accordionAberto">
+        <span class="pe-5 text-base flex-1">
+            <slot name="accordionHeader" />
+        </span>
+        <Icon name="material-symbols:keyboard-arrow-down-rounded" size="1.8em" :class="{ 'rotate-180': accordionAberto }"
+            class="accordion-icon w-12" :data-expanded="accordionAberto" />
+    </header>
+
+    <div class="accordion-body mb-5 m-3" :class="{ 'active': !accordionAberto }">
+        <slot name="accordionBody" />
+    </div>
+</template>
+
+<script setup>
+// ? Por ser Nuxt3 não precisa de import { ref  } from "vue";
+const accordionAberto = ref(false);
+</script>
+
+<style scoped>
+.accordion-icon {
+    transition: transform 0.3s ease-in-out;
+}
+
+.accordion-body {
+    height: 0;
+    overflow: hidden;
+    -webkit-transition: all 0.5s ease;
+    -moz-transition: all 0.5s ease;
+    -ms-transition: all 0.5s ease;
+    -o-transition: all 0.5s ease;
+    transition: all 1s ease;
+}
+
+.accordion-body.active {
+    min-height: fit-content;
+    height: 3rem;
+    transform: scale(1);
+}
+</style>
